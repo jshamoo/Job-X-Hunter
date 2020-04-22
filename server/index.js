d8375b2d1341150d89d2dba0c8c6523819905789
Jenny Shamoo <jenny.shamoo@gmail.com>
2020-04-22 12:12:00 -0700

    commentted out dotenv

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-require('dotenv').config();
+// require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const session = require('express-session');
